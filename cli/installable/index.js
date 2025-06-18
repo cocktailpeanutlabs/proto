8a3407f2b555c7ba0b55ad6e38b50623e14f047d
cocktailpeanut <cocktailpeanuts@proton.me>
2025-06-18 09:10:18 -0400

    update

--- cli/installable/index.js
+++ cli/installable/index.js
@@ -17,21 +17,34 @@ module.exports = {
         }, {
           title: "Install command",
           key: "install"
+        }, {
+          title: "Virtual environment",
+          description: "optional. only for python based CLI apps.",
+          key: "venv",
+          type: 'checkbox',
         }]
       }
     },
     {
       method: async (req, ondata, kernel) => {
         await fs.promises.cp(path.resolve(__dirname, "template"), req.cwd, { recursive: true })
-        await fs.promises.writeFile(path.resolve(req.cwd, "install.json"), JSON.stringify({
+
+        // install script
+        let install = {
           run: [{
             method: "shell.run",
             params: {
               message: req.input.install
             }
           }]
-        }, null, 2))
-        await fs.promises.writeFile(path.resolve(req.cwd, "start.json"), JSON.stringify({
+        }
+        if (req.input.venv) {
+          install.run[0].params.venv = "venv"
+        }
+        await fs.promises.writeFile(path.resolve(req.cwd, "install.json"), JSON.stringify(install, null, 2))
+
+        // start script
+        let start = {
           run: [{
             method: "shell.run",
             params: {
@@ -39,7 +52,13 @@ module.exports = {
               message: req.input.start
             }
           }]
-        }, null, 2))
+        }
+        if (req.input.venv) {
+          start.run[0].params.venv = "venv"
+        }
+        await fs.promises.writeFile(path.resolve(req.cwd, "start.json"), JSON.stringify(start, null, 2))
+
+        // git
         await kernel.exec({
           message: [
             "git init",
--- cli/instant/index.js
+++ cli/instant/index.js
@@ -9,21 +9,34 @@ module.exports = {
         form: [{
           title: "Enter the launch command",
           key: "command"
+        }, {
+          title: "Virtual environment",
+          description: "optional. only for python based CLI apps.",
+          key: "venv",
+          type: 'checkbox',
         }]
       }
     },
     {
       method: async (req, ondata, kernel) => {
         await fs.promises.cp(path.resolve(__dirname, "template"), req.cwd, { recursive: true })
-        await fs.promises.writeFile(path.resolve(req.cwd, "start.json"), JSON.stringify({
+
+        // start script
+        let start = {
           run: [{
             method: "shell.run",
             params: {
               input: true,
-              message: req.input.command
+              message: req.input.start
             }
           }]
-        }, null, 2))
+        }
+        if (req.input.venv) {
+          start.run[0].params.venv = "venv"
+        }
+        await fs.promises.writeFile(path.resolve(req.cwd, "start.json"), JSON.stringify(start, null, 2))
+
+        // git
         await kernel.exec({
           message: [
             "git init",
