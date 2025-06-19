963477bf7b6cd0f00c46d247e0a5419551914b4c
cocktailpeanut <cocktailpeanuts@proton.me>
2025-06-19 04:18:59 -0400

    update

--- docsify/index.js
+++ docsify/index.js
@@ -82,7 +82,7 @@ module.exports = {
         }, ondata)
 
         // update the basePath to repo
-        config.basePath = "/repo/")
+        config.basePath = "/repo/"
         await fs.promises.writeFile(path.resolve(req.cwd, "docs/docsify.config.json"), JSON.stringify(config, null, 2))
       },
       next: null,
