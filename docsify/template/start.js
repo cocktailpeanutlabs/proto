aace2c01e8b936085bb07c7fc451e5479722d5c0
cocktailpeanut <cocktailpeanuts@proton.me>
2025-06-19 01:24:09 -0400

    update

--- docsify/template/start.js
+++ docsify/template/start.js
@@ -15,7 +15,7 @@ module.exports = {
     when: "{{input && input._basePath}}",
     method: "shell.run",
     params: {
-      message: "npx -y http-server {{input._basePath}} --cors",
+      message: "npx -y http-server {{input._basePath}} --cors --cache -1",
       on: [{
         event: "/:([0-9]+)/i",
         done: true,
